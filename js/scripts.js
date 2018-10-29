903b26e55398c4f75315373df8926729e272d3df
Glen Sale and Manasa Vesala <manasa@>
2018-10-29 11:33:58 -0700

    changed code

--- js/scripts.js
+++ js/scripts.js
@@ -6,34 +6,37 @@ function Destination(location, time, notes) {
   this.notes = notes
 }
 
-Destination.prototype.location = function() {
-  return this.location;
-}
-console.log(this.location);
-Destination.prototype.time = function() {
-  return this.time;
-}
-console.log(this.time);
-Destination.prototype.notes = function() {
-  return this.notes;
-}
+var seattle = new Destination("USA","jan 5th", "Awesome");
+var portland = new Destination("USA","march 5th", "Awesome");
+var palawan = new Destination("Philippines","june 5th", "Great");
 
 
 $(document).ready(function(){
-$("#input").submit(function(event){
-  event.preventDefault();
-  var location = $("#places").val();
-  var time = $("#date").val();
-  var notes = $("#details").val();
-  var newDestination = new Destination(location, time, notes);
-
-  $("#output").text(newDestination.location);
-  $("#output").click(function(event){
+  $("#img3").click(function(event){
     event.preventDefault();
-    $("#output").append("<li>" + newDestination.time+"</li>");
-    $("#output").append("<li>"+newDestination.notes+"</li>");
+    $("#output3").append("<li>"+seattle.location+"</li>");
+    $("#output3").append("<li>" + seattle.time+"</li>");
+    $("#output3").append("<li>"+seattle.notes+"</li>");
+    $("#output3").show();
+    $("#output2").hide();
+    $("#output1").hide();
   });
-
-
+  $("#img2").click(function(event){
+    event.preventDefault();
+    $("#output2").append("<li>"+portland.location+"</li>");
+    $("#output2").append("<li>" + portland.time+"</li>");
+    $("#output2").append("<li>"+portland.notes+"</li>");
+    $("#output2").show();
+    $("#output3").hide();
+    $("#output1").hide();
+  });
+  $("#img1").click(function(event){
+    event.preventDefault();
+    $("#output1").append("<li>"+palawan.location+"</li>");
+    $("#output1").append("<li>" + palawan.time+"</li>");
+    $("#output1").append("<li>"+palawan.notes+"</li>");
+    $("#output1").show();
+    $("#output3").hide();
+    $("#output2").hide();
   });
 });
